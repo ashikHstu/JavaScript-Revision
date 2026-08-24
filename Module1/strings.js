e974507145b320997c464977eee7076546621e4c
ashikHstu <ashik.cse.hstu@gmail.com>
2026-08-24 23:21:00 +0600

    JavaScript string

---
+++ Module1/strings.js
@@ -0,0 +1,16 @@
+let firstName = "Ashik";
+let lastName = "Rahman";
+
+let fullName = firstName + " " + lastName;
+
+fullName = `${firstName} ${lastName} \nBrahmanbaria, Chittagong, Bangladesh.` // Template literal
+
+console.log(fullName);
+
+let randomstr = "I am a \"student\""
+console.log(randomstr)
+
+let address = "rajshahi";
+console.log(address.toUpperCase());
+console.log(randomstr.includes("student"));
+console.log(randomstr.length)  // not to add (), "brackets" in length
